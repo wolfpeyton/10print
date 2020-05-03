97e9cf57d28befc5957d5acd953f324b75868609
Peyton Wolf <wolfp@cs.uchicago.edu>
2020-05-03 00:00:13 -0500

    new algorithm v1

--- 10print.js
+++ 10print.js
@@ -2,17 +2,16 @@
   one possible implemenation of the 10print.org algorithm written in JavaScript
 */
 
+const chars = [['╭', '╰'], ['╮', '╯']]
 const w = process.stdout.columns
 
 function draw () {
-  setTimeout(draw, 1000)
-  let output = ''
+  setTimeout(draw, 1000 / 4)
+  let output = []
+  let lr = 0
   for (let i = 0; i < w; i++) {
-    if (Math.random() > 0.5) {
-      output += '\\'
-    } else {
-      output += '/'
-    }
+    output += chars[lr][Math.floor(Math.random() * chars[lr].length)]
+    lr ^= 1; // XOR lr with one, causes it to oscillate between 0 and 1
   }
   console.log(output)
 }
